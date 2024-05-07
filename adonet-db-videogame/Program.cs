45334c642781746eb6e746ef8f3ade38e030ae2a
Federico Maiocchi <maiocchifederico@gmail.com>
2024-05-07 17:51:55 +0200

    ADD: insertVideogame

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            VideogameManager.GetVideogameByID();
+
+            Console.WriteLine("Inserisci il nome del videogioco:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Inserisci la descrizione del videogioco:");
+            string overview = Console.ReadLine();
+
+            Console.WriteLine("Inserisci la data di rilascio del videogioco (YYYY-MM-DD):");
+            if (!DateTime.TryParse(Console.ReadLine(), out DateTime releaseDate))
+            {
+                Console.WriteLine("Data non valida.");
+                return;
+            }
+
+            DateTime createdAt = DateTime.Now;
+            DateTime updatedAt = DateTime.Now;
+
+            Console.WriteLine("Inserisci l'ID della casa di produzione del videogioco:");
+            if (!int.TryParse(Console.ReadLine(), out int softwareHouseId))
+            {
+                Console.WriteLine("ID non valido.");
+                return;
+            }
+
+            VideogameManager.InsertVideogame(name, overview, releaseDate, createdAt, updatedAt, softwareHouseId);
+
+
+            
+
         }
+
+
+       
     }
 }
--- adonet-db-videogame/Videogame.cs
+++ adonet-db-videogame/Videogame.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +27,15 @@ namespace adonet_db_videogame
         public DateTime UpdatedAt {  get; set; }
 
         public int SoftwareHouse {  get; set; }
-        
-        public Videogame(int id, string name, string overview, DateTime releaseDate, DateTime createAt, DateTime updatedAt, int softwareHouse) 
-        {
 
-            Id = id;
+        public static int countId = 0; 
+
+
+        
+        public Videogame(string name, string overview, DateTime releaseDate, DateTime createAt, DateTime updatedAt, int softwareHouse) 
+        {
+            
+            Id = countId++;
             Name = name;
             Overview = overview;
             ReleaseDate = releaseDate;
@@ -41,15 +48,72 @@ namespace adonet_db_videogame
 
     public static class VideogameManager 
     {
-        static void InsertVideogame(int id, string name, string overview, DateTime release_date, DateTime created_at, DateTime updated_at, int software_house)
+        // stringa di connessione per connetterci al db
+        static string stringConnection = "Data Source=localhost;Initial Catalog=db_videogames;Integrated Security=True;";
+
+        //funzione per inserire un nuovo videogioco
+        public static void InsertVideogame(string name, string overview, DateTime release_date, DateTime created_at, DateTime updated_at, int software_house_id)
         {
-            Videogame NewVideogame = new Videogame(id, name, overview, release_date, created_at, updated_at, software_house);
+            Videogame NewVideogame = new Videogame(name, overview, release_date, created_at, updated_at, software_house_id);
+
+            using SqlConnection sqlConnection = new SqlConnection(stringConnection);
+
+            //stringa query
+            string query = "INSERT INTO videogames(name, overview, release_date, created_at, updated_at, software_house_id) " +
+                "VALUES (@name, @overview, @release_date, @created_at, @updated_at, @software_house_id)";
+
+            try
+            {
+                sqlConnection.Open();
+
+                using SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                cmd.Parameters.Add(new SqlParameter("@name", name));
+                cmd.Parameters.Add(new SqlParameter("@overview", overview));
+                cmd.Parameters.Add(new SqlParameter("@release_date", release_date));
+                cmd.Parameters.Add(new SqlParameter("@created_at", created_at));
+                cmd.Parameters.Add(new SqlParameter("@updated_at", updated_at));
+                cmd.Parameters.Add(new SqlParameter("software_house_id", software_house_id));
+
+                int affectedRows = cmd.ExecuteNonQuery();
+
+            } catch( Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+
 
         }
 
-        static void GetVideogameByID()
+        public static void GetVideogameByID()
         {
+            
 
+            using SqlConnection sqlConnection = new SqlConnection(stringConnection);
+
+            try
+            {
+                sqlConnection.Open();
+                string query = @"SELECT *
+                FROM videogames 
+                WHERE id = '5'";
+
+                using SqlCommand cmd = new SqlCommand(query, sqlConnection);
+
+                using SqlDataReader reader = cmd.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    int indexVideogame = reader.GetOrdinal("id");
+                    string nameVideogame = reader.GetString("name");
+                    Console.WriteLine(nameVideogame);
+                   
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
         }
 
         static void SearchVideogameByName()
